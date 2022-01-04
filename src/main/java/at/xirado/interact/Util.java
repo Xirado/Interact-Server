33ea772061b40fd7ade9bca17bdb8ec1cb9b0e79
Marcel Korzonek <marcel.korzonek@htlstp.at>
2022-01-04 22:54:36 +0100

    Test again

--- src/main/java/at/xirado/interact/Util.java
+++ src/main/java/at/xirado/interact/Util.java
@@ -6,6 +6,12 @@ public class Util
 {
     public static byte[] hexToBytes(String hexString)
     {
-        return new BigInteger(hexString, 16).toByteArray();
+        int len = hexString.length();
+        byte[] data = new byte[len / 2];
+        for (int i = 0; i < len; i += 2) {
+            data[i / 2] = (byte) ((Character.digit(hexString.charAt(i), 16) << 4)
+                    + Character.digit(hexString.charAt(i+1), 16));
+        }
+        return data;
     }
 }
